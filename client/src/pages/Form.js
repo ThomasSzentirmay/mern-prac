d4f42115f32c599ff2787843789d8d129836aca1
Thomas Szentirmay <tlszent@outlook.com>
2023-08-11 00:49:00 +1000

    new method to pull in old state

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -1,9 +1,13 @@
+import {useStore} from '../store';
+
 function Form(props) {
+    const {title} = useStore();
+
     return (
         <>
             <h1>Add a Task</h1>
 
-            
+            <p>{title}</p>
         </>
     )
 }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,24 @@
-import {useStore} from './store';
+import {useStore} from '../store';
 
 function Landing(props) {
-    const {title} = useStore();
+    const {title, setGlobalState} = useStore();
+
+    const changeTitle = () => {
+        setGlobalState(oldState => {
+            return {
+                ...oldState,
+                title: 'Something else'
+            }
+        });
+    }
 
     return (
         <>
             <h1>Task Manager</h1>
 
             <p>{title}</p>
+
+            <button onClick={changeTitle}>Change</button>
         </>
     )
 }

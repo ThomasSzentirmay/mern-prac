69407302410a1cb43d346129af223be3d4ac3461
Thomas Szentirmay <tlszent@outlook.com>
2023-08-11 03:39:47 +1000

    setting up apollo grapghql server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,28 @@
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+
+const { typeDefs, resolvers } = require('./schema');
+
 const express = require('express');
 
 const app = express();
+const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 3333;
+
+async function startServer() {
+    const server = new ApolloServer<MyContext>({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+      });
+      // Ensure we wait for our server to start
+      await server.start();
+}
 
 // import typeDefs and resolvers
 

941ea50c826011b7a2fae218c2e913b0f70890dd
Thomas Szentirmay <tlszent@outlook.com>
2023-08-11 04:24:38 +1000

    set up basic graphql queries

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,3 +1,14 @@
+const Task = require('../models/Task');
+
 module.exports = {
-    
+    Query: {
+        sayHello() {
+            return 'Hi there!';
+        },
+        async getTasks() {
+            const tasks = await Task.find();
+            
+            return tasks;
+        }
+    }
 }
